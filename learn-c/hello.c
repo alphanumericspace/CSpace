2da352c9a907fd9b984c90162ff4fd8cd40e896f
Alphanumeric Space <alphanumericspace@gmail.com>
2024-02-14 20:29:34 +0700

    Add learn-c

---
+++ learn-c/hello.c
@@ -0,0 +1,60 @@
+/*
+	Copyright (c) 2024 Alphanumeric Space
+
+	GitHub: https://github.com/alphanumericspace/CSpace
+*/
+
+#include <stdio.h>
+
+/*
+	#include <stdio.h> is a preprocessor directive
+	in the C language used to include the standard
+	input-output header file stdio.h into the program.
+	This header contains declarations of standard
+	functions for input and output, such as
+	printf() and scanf(). By including stdio.h,
+	we can use these functions in a C program
+	without needing to define them ourselves.
+*/
+
+int main() {
+
+	printf("Hello, World!\n");
+
+	/*
+		The printf("Hello, World!"); statement in C
+		is used to display the string "Hello, World!"
+		on the screen. The printf function is part
+		of the standard input-output library (stdio.h)
+		and is commonly used to output formatted data
+		to the standard output (usually the console).
+		In this case, "Hello, World!" is a string
+		literal enclosed in double quotes, and it will
+		be printed exactly as it appears, including
+		the punctuation and spaces
+	*/
+	
+	return 0;
+
+	/*
+		The statement return 0; in the main() function
+		indicates that the program has successfully
+		terminated. The value 0 returned is the exit
+		code of the program, typically indicating
+		successful execution without any errors.
+		It's a common convention in many operating
+		systems that a return value of 0 signifies
+		successful program execution.
+	*/
+}
+
+/*
+	In C, int main() {} is the entry point of a C
+	program. It defines the main function, which
+	serves as the starting point of program execution.
+	The int before main indicates that the function
+	returns an integer value to the operating system
+	when the program terminates. Within the curly
+	braces {}, you write the statements that make up
+	the body of the main function.
+*/
---
+++ learn-c/variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a = 0, b = 0;
+	printf("%d %d\n", a, b);
+
+	a = 10;
+	b = 20;
+	printf("%d %d\n", a, b);
+
+	return 0;
+}
